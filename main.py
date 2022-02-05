2672852f855b67f9004005c7588366f07411fc21
Ofir Meir <ofiridan22@gmail.com>
2022-02-05 21:26:09 +0200

    added game functionality - got user input, checked if it was previously guessed, handle correct guess

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 import turtle
+import pandas
+
+FONT = ("Courier", "8", "normal")
 
 screen = turtle.Screen()
 screen.title("US States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
+
+previously_corrected_guesses = []
+
+df = pandas.read_csv("./50_states.csv")
+states_list = df["state"].to_list()
+
+while previously_corrected_guesses < 50:
+    answer = screen.textinput(title=f" {len(previously_corrected_guesses)}/50 Guess the state",
+                              prompt="What is your next state's name?")
+    answer_titled = answer.title()
+    if answer_titled in states_list:
+        if answer_titled not in previously_corrected_guesses:
+            previously_corrected_guesses.append(answer_titled)
+            state_turtle = turtle.Turtle()
+            state_turtle.penup()
+            state_turtle.hideturtle()
+            state_df = df[df["state"] == answer_titled]
+            state_turtle.goto(int(state_df["x"]), int(state_df["y"]))
+            state_turtle.write(answer_titled, align="center", font=FONT)
+
+screen.exitonclick()
